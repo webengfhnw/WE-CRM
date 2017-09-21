948d889f128f49bc38aee85d336c5377dc88e11b
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-09-21 14:15:09 +0200

    Fix: adding mobile to customer edit view

--- stage02/view/customerEdit.php
+++ stage02/view/customerEdit.php
@@ -28,6 +28,12 @@
                 <input class="form-control" type="email" name="email">
             </div>
         </div>
+        <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-addon"><span>Mobile </span></div>
+                    <input class="form-control" type="text" name="mobile">
+                </div>
+        </div>
         <div class="btn-group" role="group">
             <button class="btn btn-default" type="submit"> <i class="fa fa-save"></i></button>
         </div>
