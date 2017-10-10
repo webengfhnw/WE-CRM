d80f7f27a41906dd6d85b978770b294966d80a1c
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-10-10 16:18:03 +0200

    Stage 05 without DB

---
+++ stage05nodb/index.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreas.martin
+ * Date: 12.09.2017
+ * Time: 21:30
+ */
+require_once("config/Autoloader.php");
+require_once("view/layout.php");
+
+use router\Router;
+
+session_start();
+
+$authFunction = function () {
+    if (isset($_SESSION["agentLogin"])) {
+        return true;
+    }
+    Router::redirect("/login");
+    return false;
+};
+
+$errorFunction = function () {
+    Router::errorHeader();
+    require_once("view/404.php");
+};
+
+    Router::route("GET", "/login", function () {
+        require_once("view/agentLogin.php");
+    });
+
+    Router::route("GET", "/register", function () {
+        require_once("view/agentEdit.php");
+    });
+
+    Router::route("POST", "/register", function () {
+        Router::redirect("/logout");
+    });
+
+    Router::route("POST", "/login", function () {
+        $_SESSION['agentLogin']=$_POST['email'];
+        Router::redirect("/");
+    });
+
+    Router::route("GET", "/logout", function () {
+        session_destroy();
+        Router::redirect("/login");
+    });
+
+    Router::route_auth("GET", "/", $authFunction, function () {
+        layoutSetContent("customers.php");
+    });
+
+    Router::route_auth("GET", "/agent/edit", $authFunction, function () {
+        require_once("view/agentEdit.php");
+    });
+
+    Router::route_auth("GET", "/customer/create", $authFunction, function () {
+        layoutSetContent("customerEdit.php");
+    });
+
+    Router::route_auth("GET", "/customer/edit", $authFunction, function () {
+        layoutSetContent("customerEdit.php");
+    });
+
+    Router::route_auth("GET", "/customer/delete", $authFunction, function () {
+        $id = $_GET["id"];
+        Router::redirect("/");
+    });
+
+    Router::route_auth("POST", "/customer/update", $authFunction, function () {
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $mobile = $_POST["mobile"];
+        Router::redirect("/");
+    });
+
+Router::call_route($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO'], $errorFunction);
