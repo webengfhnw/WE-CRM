d519fe2fd539afd161adc6d0623c1bb19d47667f
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-14 08:58:04 +0100

    Update

---
+++ testbed/stage09/xssSimple.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreas.martin
+ * Date: 14.11.2017
+ * Time: 08:52
+ *
+ * TODO: enter "<script>alert("hacked")</script>" to test XSS
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+<span>
+    <?php ?>
+</span>
+</body>
+</html>
