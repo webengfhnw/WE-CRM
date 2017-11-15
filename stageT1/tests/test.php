37a04887644857a2ed4309e322d78d3d12b3a95f
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-15 21:24:39 +0100

    test

--- stageT1/tests/test.php
+++ stageT1/tests/test.php
@@ -6,3 +6,8 @@ require('../vendor/autoload.php');
 use HelloWorld\Greetings;
 
 echo Greetings::sayHelloWorld();
+echo "<br>";
+strpos($_SERVER['SERVER_PORT'],"80") ? $serverPort = "" : $serverPort = ":" . $_SERVER['SERVER_PORT'];
+echo $serverPort;
+echo "<br>";
+echo $_SERVER['SERVER_PORT'];
