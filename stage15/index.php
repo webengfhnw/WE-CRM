e803fe508243efabb6c72e31fa08b5c774f24c0e
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-17 18:17:34 +0100

    CORS

--- stage15/index.php
+++ stage15/index.php
@@ -151,10 +151,10 @@ Router::route_auth("DELETE", "/api/customer/{id}", $authAPITokenFunction, functi
 });
 
 try {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    header("Access-Control-Allow-Headers: Authorization, Location, Origin, Content-Type, X-Requested-With");
     if($_SERVER['REQUEST_METHOD']=="OPTIONS") {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        header("Access-Control-Allow-Headers: Authorization, Location, Origin, Content-Type, X-Requested-With");
         header("HTTP/1.0 204 No Response", true, 204);
     } else {
         Router::call_route($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
