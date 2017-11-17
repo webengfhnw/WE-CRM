cf78402444f04a0dee1a595a4c03237207a2ce19
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-17 18:08:45 +0100

    CORS

--- stage15/index.php
+++ stage15/index.php
@@ -108,11 +108,11 @@ Router::route_auth("GET", "/customer/pdf", $authFunction, function () {
     PDFController::generatePDFCustomers();
 });
 
-if($_SERVER['REQUEST_METHOD']==="OPTIONS") {
+if($_SERVER['REQUEST_METHOD']=="OPTIONS") {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
     header("Access-Control-Allow-Headers: Authorization, Location, Origin, Content-Type, X-Requested-With");
-    header("HTTP/1.0 204 No Response");
+    header("HTTP/1.0 204 No Response", true, 204);
 }
 
 
