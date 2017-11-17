32398d418ca1a9fb5e21cbedebb6441a58f8275f
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-17 18:00:55 +0100

    CORS

--- stage15/index.php
+++ stage15/index.php
@@ -110,7 +110,7 @@ Router::route_auth("GET", "/customer/pdf", $authFunction, function () {
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, HEAD, OPTIONS");
-header("Access-Control-Allow-Headers: Authorization, Location, Origin, Content-Type");
+header("Access-Control-Allow-Headers: Authorization, Location, Origin, Content-Type, X-Requested-With");
 
 $authAPIBasicFunction = function () {
     if (ServiceEndpoint::authenticateBasic())
