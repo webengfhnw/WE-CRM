0033ccbdff5e35cb8a928ca86635ee6893f3184c
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-17 18:29:03 +0100

    CORS

--- testbed/stage15/index.php
+++ testbed/stage15/index.php
@@ -10,6 +10,8 @@ require_once("config/Autoloader.php");
 use router\Router;
 use service\ServiceEndpoint;
 use http\HTTPException;
+use http\HTTPHeader;
+use http\HTTPStatusCode;
 
 Router::route("GET", "/api/customer", function () {
     ServiceEndpoint::findAllCustomer();
@@ -32,7 +34,14 @@ Router::route("DELETE", "/api/customer/{id}", function ($id) {
 });
 
 try {
-    Router::call_route($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
+    HTTPHeader::setHeader("Access-Control-Allow-Origin: *");
+    HTTPHeader::setHeader("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    HTTPHeader::setHeader("Access-Control-Allow-Headers: Authorization, Location, Origin, Content-Type, X-Requested-With");
+    if($_SERVER['REQUEST_METHOD']=="OPTIONS") {
+        HTTPHeader::setStatusHeader(HTTPStatusCode::HTTP_204_NO_CONTENT);
+    } else {
+        Router::call_route($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
+    }
 } catch (HTTPException $exception) {
     $exception->getHeader();
 }
