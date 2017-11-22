db45f75d80045f8b77cb6d2aa3923941a07fbf8c
Andreas Martin <andreas.martin@andreasmartin.ch>
2017-11-22 14:05:40 +0100

    MySQL DDL

---
+++ modelling/sql/WECRM.mysql.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Customer (
+  ID      int(10) NOT NULL AUTO_INCREMENT, 
+  Name    varchar(255) NOT NULL, 
+  Email   varchar(255) NOT NULL, 
+  Mobile  varchar(255) NOT NULL, 
+  AgentID int(10) NOT NULL, 
+  PRIMARY KEY (ID));
+CREATE TABLE Agent (
+  ID       int(10) NOT NULL AUTO_INCREMENT, 
+  Name     varchar(255) NOT NULL, 
+  Email    varchar(255) NOT NULL, 
+  Password varchar(255) NOT NULL, 
+  PRIMARY KEY (ID));
+CREATE TABLE AuthToken (
+  ID         int(10) NOT NULL AUTO_INCREMENT, 
+  AgentID    int(10) NOT NULL, 
+  Selector   varchar(255) NOT NULL, 
+  Validator  varchar(255) NOT NULL, 
+  Expiration timestamp NOT NULL, 
+  Type       int(10) NOT NULL, 
+  PRIMARY KEY (ID));
+ALTER TABLE Customer ADD INDEX AgentCustomer (AgentID), ADD CONSTRAINT AgentCustomer FOREIGN KEY (AgentID) REFERENCES Agent (ID);
+ALTER TABLE AuthToken ADD INDEX AgentToken (AgentID), ADD CONSTRAINT AgentToken FOREIGN KEY (AgentID) REFERENCES Agent (ID);
