69a862930505acf3ce4edfa62db641bf1c3d744f
Andreas Martin <andreas.martin@andreasmartin.ch>
2018-12-03 15:50:53 +0100

    Test

--- testbed/stage13/indexTemp2.php
+++ testbed/stage13/indexTemp2.php
@@ -6,54 +6,53 @@
  * Time: 14:04
  */
 
-send();
-send();
-send();
-
-function send()
+class Send
 {
-    $jsonObj = createPDFJSONObj();
-    $jsonObj->user = $_ENV["HYPDF_USER"];
-    $jsonObj->password = $_ENV["HYPDF_PASSWORD"];
-    $jsonObj->content = "<html><p>Hello, world!</p></html>";
 
-    $options = ["http" => [
-        "method" => "POST",
-        "header" => ["Content-Type: application/json"],
-        "content" => json_encode($jsonObj)
-    ]];
-    $context = stream_context_create($options);
-    $response = file_get_contents("https://www.hypdf.com/htmltopdf", false, $context);
+    public static function send()
+    {
+        $jsonObj = createPDFJSONObj();
+        $jsonObj->user = $_ENV["HYPDF_USER"];
+        $jsonObj->password = $_ENV["HYPDF_PASSWORD"];
+        $jsonObj->content = "<html><p>Hello, world!</p></html>";
 
-    $jsonObj = createEmailJSONObj();
-    $jsonObj->personalizations[0]->to[0]->email = "andreas.martin@fhnw.ch";
-    $jsonObj->subject = "Hello, World!";
-    $jsonObj->content[0]->value = "<html><p>Hello, world!</p></html>";
-    $jsonObj->attachments[0]->filename = "file.pdf";
-    $jsonObj->attachments[0]->content = base64_encode($response);
+        $options = ["http" => [
+            "method" => "POST",
+            "header" => ["Content-Type: application/json"],
+            "content" => json_encode($jsonObj)
+        ]];
+        $context = stream_context_create($options);
+        $response = file_get_contents("https://www.hypdf.com/htmltopdf", false, $context);
+
+        $jsonObj = createEmailJSONObj();
+        $jsonObj->personalizations[0]->to[0]->email = "andreas.martin@fhnw.ch";
+        $jsonObj->subject = "Hello, World!";
+        $jsonObj->content[0]->value = "<html><p>Hello, world!</p></html>";
+        $jsonObj->attachments[0]->filename = "file.pdf";
+        $jsonObj->attachments[0]->content = base64_encode($response);
 
 
-    $options = ["http" => [
-        "method" => "POST",
-        "header" => ["Content-Type: application/json",
-            "Authorization: Bearer ".$_ENV["SENDGRID_APIKEY"].""],
-        "content" => json_encode($jsonObj)
-    ]];
-    $context = stream_context_create($options);
-    $response = file_get_contents("https://api.sendgrid.com/v3/mail/send", false, $context);
-    if (strpos($http_response_header[0], "202"))
-        echo json_decode($response);
+        $options = ["http" => [
+            "method" => "POST",
+            "header" => ["Content-Type: application/json",
+                "Authorization: Bearer " . $_ENV["SENDGRID_APIKEY"] . ""],
+            "content" => json_encode($jsonObj)
+        ]];
+        $context = stream_context_create($options);
+        $response = file_get_contents("https://api.sendgrid.com/v3/mail/send", false, $context);
+        if (strpos($http_response_header[0], "202"))
+            echo json_decode($response);
 
-}
+    }
 
-function createPDFJSONObj()
-{
-    return json_decode('{"content": "HTML", "user": "HYPDF_USER", "password": "YOUR_HYPDF_PASSWORD", "test": "true"}');
-}
+    public static function createPDFJSONObj()
+    {
+        return json_decode('{"content": "HTML", "user": "HYPDF_USER", "password": "YOUR_HYPDF_PASSWORD", "test": "true"}');
+    }
 
-function createEmailJSONObj()
-{
-    return json_decode('{
+    public static function createEmailJSONObj()
+    {
+        return json_decode('{
           "personalizations": [
             {
               "to": [
@@ -82,4 +81,9 @@ function createEmailJSONObj()
             }
           ]
         }');
+    }
 }
+
+Send::send();
+Send::send();
+Send::send();
